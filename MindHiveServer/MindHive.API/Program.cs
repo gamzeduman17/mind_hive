9720859e8d84235f9f767a48f345e755961cccec
Gamze D <gamzeduman1729@gmail.com>
2025-10-08 21:33:11 +0300

    dbcontext-appsettings changed

--- MindHiveServer/MindHive.API/Program.cs
+++ MindHiveServer/MindHive.API/Program.cs
@@ -2,31 +2,38 @@ using MindHive.Application.ApiServices;
 using MindHive.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//configuration
+ConfigurationManager configuration = builder.Configuration;
+//add services
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-
-// Add Infrastructure services (DbContext, Repositories)
-builder.Services.AddInfrastructure(builder.Configuration);
-
-// Add Application services
+// infrastructure
+builder.Services.AddInfrastructure(configuration);
+//layer services
 builder.Services.AddScoped<UserService>();
 builder.Services.AddSingleton<JwtService>();
-
+//cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+//build app
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+// middleware pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseHttpsRedirection();
 
-// Map attribute-routed controllers (e.g., api/Auth/login)
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
